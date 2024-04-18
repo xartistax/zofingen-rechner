ea4df7e6ea1ac0dff0019145b468b45fb97ec735
Artista <demianfueglistaler@icloud..com>
2024-04-18 23:30:32 +0200

    using full paths fix

--- src/app/components/PDF/PDFCreate.tsx
+++ src/app/components/PDF/PDFCreate.tsx
@@ -1,38 +1,31 @@
-
-
 "use server"
 import ReactPDF from '@react-pdf/renderer';
 import PDFFile from './PDFFile';
 import { DebugValues } from '../Survey/SurveyComponent';
 import fs from 'fs';
-import { put } from '@vercel/blob';
 import { uploadPDFToVercel } from '@/app/utils/uploadPDFToVercel';
 import { pdfsDirectory } from '@/app/utils/calculations';
+import path from 'path'; // Import path module
 
 export const PDFCreator = async (createPDF: boolean, uuid: string, debugValues: DebugValues) => {
     try {
-      
+        // Construct the absolute path to the PDF file
+        const pdfFilePath = path.join(pdfsDirectory, `${uuid}.pdf`);
 
-        const pdfFilePath = `${pdfsDirectory}/${uuid}.pdf`;
+        // Ensure that the directory exists, create it if it doesn't
+        const pdfDirectoryPath = path.dirname(pdfFilePath);
+        if (!fs.existsSync(pdfDirectoryPath)) {
+            fs.mkdirSync(pdfDirectoryPath, { recursive: true });
+        }
+
+        // Proceed with rendering the PDF
         await ReactPDF.render(<PDFFile uuid={uuid} debugValues={debugValues} />, pdfFilePath);
 
-        
-    
-        
- 
-
-
+        // Upload the PDF to Vercel if necessary
+       
     } catch (error) {
-        console.error('Error uploading PDF:', error);
+        console.error('Error rendering or uploading PDF:', error);
     } 
 };
 
 export default PDFCreator;
-
-
-
-
-
-
-
-
