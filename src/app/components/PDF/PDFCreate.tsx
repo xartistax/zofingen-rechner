9ea6f86e77af8f7437ebc8deb66326d1c6d476f8
Artista <demianfueglistaler@icloud..com>
2024-04-19 11:16:22 +0200

    changes 001

--- src/app/components/PDF/PDFCreate.tsx
+++ src/app/components/PDF/PDFCreate.tsx
@@ -5,19 +5,20 @@ import { DebugValues } from '../Survey/SurveyComponent';
 import fs from 'fs-extra';
 import { uploadPDFToVercel } from '@/app/utils/uploadPDFToVercel';
 import { pdfsDirectory } from '@/app/utils/calculations';
-import path from 'path'; // Import path module
+import path, { join } from 'path'; // Import path module
 import ReactPDF from '@react-pdf/renderer';
 import { mkdirp } from 'mkdirp'
 
 
 
-
+ 
 
 
 export const PDFCreator = async (createPDF: boolean, uuid: string, debugValues: DebugValues) => {
     try {
 
-        const dir = path.resolve(process.cwd(), 'tmp', uuid);
+        const dir= join(process.cwd(), '/tmp', uuid);
+
         const made = mkdirp.sync(dir)
         console.log(`made directories, starting with ${made}`)
 
